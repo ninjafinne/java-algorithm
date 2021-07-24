afe458b3cfc790a88ea30fc8f99fcc684ff3dbde
Mikko Salomäki <ms@datarespons.se>
2021-07-24 22:10:40 +0200

    Add InsertionSort.sortDescend()

--- tests/TestInsertionSort.java
+++ tests/TestInsertionSort.java
@@ -6,7 +6,15 @@ class TestInsertionSort {
 	void testSort() {
 		for (DataSet d : DataSet.dataSets) {
 			int[] out = InsertionSort.sort(d.data);
-			assertArrayEquals(d.sortInc, out);
+			assertArrayEquals(d.sortInc, out, d.msg);
+		}
+	}
+	
+	@Test
+	void testSortDescend() {
+		for (DataSet d : DataSet.dataSets) {
+			int[] out = InsertionSort.sortDescend(d.data);
+			assertArrayEquals(d.sortDec, out, d.msg);
 		}
 	}
 }
